8fbc66f637261f5ea07cb3857368792e6034a994
Yuri <yuricoelho33@gmail.com>
2021-05-17 10:29:52 -0300

    Testando o código refatorado.

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-from cpf_cnpj import CpfCnpj
+from cpf_cnpj import Documento
 from validate_docbr import CNPJ
 
 # cpf_um = Cpf("15316264754")
 # print(cpf_um)
 
-exemplo_cnpj = "35379838000112"
-exemplo_cpf = "32007832062"
+# exemplo_cnpj = "35379838000112"
+exemplo_cpf = "3200783204462"
 # cnpj = CNPJ()
 # print(cnpj.validate(exemplo_cnpj))
-documento = CpfCnpj(exemplo_cpf, "cpf")
+documento = Documento.cria_documento(exemplo_cpf)
 print(documento)
