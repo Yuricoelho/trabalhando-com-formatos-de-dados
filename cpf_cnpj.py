08a686cb00d7b43d00c6773a1afd67c26cd754b8
Yuri <yuricoelho33@gmail.com>
2021-05-17 10:09:45 -0300

    Adicionando a máscar do CNPJ.

--- cpf_cnpj.py
+++ cpf_cnpj.py
@@ -20,7 +20,10 @@ class CpfCnpj:
            raise ValueError("Documento inválido!")
 
     def __str__(self):
-        return self.format_cpf()
+        if self.tipo_documento == "cpf":
+            return self.format_cpf()
+        elif self.tipo_documento == "cnpj":
+            return self.format_cnpj()
 
     def cpf_eh_valido(self, cpf):
         if len(cpf) == 11:
@@ -32,6 +35,10 @@ class CpfCnpj:
     def format_cpf(self):
         mascara = CPF()
         return mascara.mask(self.cpf)
+
+    def format_cnpj(self):
+        mascara = CNPJ()
+        return mascara.mask(self.cnpj)
 
     def cnpj_eh_valido(self, cnpj):
         if len(cnpj) == 14:
