8a6c70367009d7d63b61bd38d416b828995c7f47
Yuri <yuricoelho33@gmail.com>
2021-05-17 12:16:56 -0300

    Testando a formatação das datas.

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 from datetime import datetime, timedelta
 from datas_br import DatasBr
 
-cadastro = DatasBr()
-print(cadastro)
-
-# hoje = datetime.today()
-# hoje_formatado = hoje.strftime("%d/%m/%y %H:%M")
-# print(hoje)
-# print(hoje_formatado)
-# print(type(hoje_formatado))
+hoje = DatasBr()
+print(hoje.tempo_cadastro())
 
