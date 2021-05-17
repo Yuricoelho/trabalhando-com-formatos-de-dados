01d3432941e3a4ac9828aee60d0a221221c25e44
Yuri <yuricoelho33@gmail.com>
2021-05-17 10:02:15 -0300

    Testando a validação do CNPJ.

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-from Cpf import Cpf
+from cpf_cnpj import CpfCnpj
+from validate_docbr import CNPJ
 
-cpf_um = Cpf("15316264754")
-print(cpf_um)
+# cpf_um = Cpf("15316264754")
+# print(cpf_um)
 
-
+exemplo_cnpj = "35379838000112"
+# cnpj = CNPJ()
+# print(cnpj.validate(exemplo_cnpj))
+documento = CpfCnpj(exemplo_cnpj, "cnpj")
