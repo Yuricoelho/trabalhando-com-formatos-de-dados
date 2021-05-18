a5c7f893a55f9dfe3a627de8f4f4834a6cfc11c1
Yuri <yuricoelho33@gmail.com>
2021-05-17 22:28:33 -0300

    Testando a API resquests para lidar com o CEP.

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
+import requests
 from acesso_cep import BuscaEndereco
-
-cep = 25870145
+cep = "01001000"
 objeto_cep = BuscaEndereco(cep)
-print(objeto_cep)
+# r = requests.get("https://viacep.com.br/ws/01001000/json/")
+# print(r.text)
+
+a = objeto_cep.acessa_via_cep()
+print(a)
 
 
 
